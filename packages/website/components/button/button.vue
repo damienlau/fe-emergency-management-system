0b5dc15da0c8d503ce9bd83c666f96b7ce352429
Damien Lau <damienlaus@icloud.com>
2021-07-15 19:56:39 +0800

    feat: 封装按钮组件

---
+++ packages/website/components/button/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-button class="lh-22 px-12 py-6">
+    <slot name="icon" />
+    <slot />
+  </el-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DottmedButton",
+});
+</script>
