381967db066f0e597c01cf0c970055c44a4349d6
Damien Lau <damienlaus@icloud.com>
2021-07-15 19:54:48 +0800

    feat: 创建公共样式库

--- packages/website/App.tsx
+++ packages/website/App.tsx
@@ -1,7 +1,22 @@
 import { defineComponent } from "vue";
+import DottmedButton from "./components/button";
 
 export default defineComponent({
   setup() {
-    return () => <el-button>ddd</el-button>;
+    return () => (
+      <el-container>
+        <el-header>
+          <DottmedButton type="primary">物资入库</DottmedButton>
+          <DottmedButton type="danger">物资入库</DottmedButton>
+          <DottmedButton disabled type="primary">
+            物资入库
+          </DottmedButton>
+          <DottmedButton plain type="primary">
+            物资入库
+          </DottmedButton>
+        </el-header>
+        <el-main></el-main>
+      </el-container>
+    );
   },
 });
--- packages/website/dottmed.ts
+++ packages/website/dottmed.ts
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
 import App from "./App";
 import ElementPlus from "element-plus";
+// import { store, key } from "./store";
+
+import "common/scss/dottmed.scss";
 
 const dottmed = createApp(App);
 
 dottmed.use(ElementPlus);
+// dottmed.use(store, key);
 
 dottmed.mount("#dottmed");
